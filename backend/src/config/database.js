044ce95e5a3ac8a949f2f6095eca1e1b10f8c7fe
Arya Bhiwapurkar <aaryanspl@gmail.com>
2026-04-25 16:42:21 +0530

    added db details in .env

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,9 +2,10 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'movie_booking_db',
-    password: 'postgres',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
+
